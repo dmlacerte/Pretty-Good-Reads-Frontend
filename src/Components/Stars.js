a7a794a9bea582a1a6fab68fbd45545b8502250c
dmlacerte <dmlacerte@gmail.com>
2022-06-03 18:42:13 -0400

    formatting

--- src/Components/Stars.js
+++ src/Components/Stars.js
@@ -1,36 +1,21 @@
-import React, {useEffect, useState, useContext} from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import styles from './css/Stars.module.css'
 import axios from 'axios'
 import UserContext from '../UserContext'
 
-const Stars = ({userId, bookId, avg}) => {
-    const {reRender, book} = useContext(UserContext)
+const Stars = ({ userId, bookId, avg }) => {
+    const { reRender, book } = useContext(UserContext)
     const [starRating, setStarRating] = useState(null)
 
     useEffect(() => {
-        //if setting community look at all reviews
-        //else look at the users review
-        // if (avg) {
-        //     axios.get(process.env.NODE_ENV === 'production'
-        //     ? process.env.REACT_APP_BACK_END_PROD + `/rate/book/${book['_id']}`
-        //     : process.env.REACT_APP_BACK_END_DEV + `/rate/book/${book['_id']}`)
-        //     .then(res => {
-        //         if(!res.data) setStarRating(null);
-        //             else {
-        //                 let sum = res.data.reduce((n, {score}) => n + score, 0)
-        //                 let finalAvg = sum / res.data.length
-        //                 setStarRating(Math.round(finalAvg))
-        //             }
-        //     })
-        // } else {
         if (!avg) {
             axios.get(process.env.NODE_ENV === 'production'
-            ? process.env.REACT_APP_BACK_END_PROD + `/rate/${userId}/${bookId}`
-            : process.env.REACT_APP_BACK_END_DEV + `/rate/${userId}/${bookId}`)
-            .then(res => {
-                if(!res.data) setStarRating(null);
-                else setStarRating(res.data.score)
-            })
+                ? process.env.REACT_APP_BACK_END_PROD + `/rate/${userId}/${bookId}`
+                : process.env.REACT_APP_BACK_END_DEV + `/rate/${userId}/${bookId}`)
+                .then(res => {
+                    if (!res.data) setStarRating(null)
+                    else setStarRating(res.data.score)
+                })
         }
     }, [reRender])
 
@@ -38,36 +23,36 @@ const Stars = ({userId, bookId, avg}) => {
     useEffect(() => {
         if (avg) {
             axios.get(process.env.NODE_ENV === 'production'
-            ? process.env.REACT_APP_BACK_END_PROD + `/rate/book/${book['_id']}`
-            : process.env.REACT_APP_BACK_END_DEV + `/rate/book/${book['_id']}`)
-            .then(res => {
-                if(!res.data) setStarRating(null);
+                ? process.env.REACT_APP_BACK_END_PROD + `/rate/book/${book['_id']}`
+                : process.env.REACT_APP_BACK_END_DEV + `/rate/book/${book['_id']}`)
+                .then(res => {
+                    if (!res.data) setStarRating(null)
                     else {
-                        let sum = res.data.reduce((n, {score}) => n + score, 0)
+                        let sum = res.data.reduce((n, { score }) => n + score, 0)
                         let finalAvg = sum / res.data.length
                         setStarRating(Math.round(finalAvg))
                     }
-            })
+                })
         }
     }, [book, reRender])
 
-  return (
-    <>
-    <div className={avg ? 'communityRating' : 'stars'}>
-        {[...Array(5)].map((star, idx) => {
-            idx += 1
-            return (
-                <span 
-                        key ={idx}
-                        className={`${styles.stars} ${idx <= starRating ? styles.on : styles.off}`}
-                    >
-                        &#9733;
-                </span>
-            )
-        })}
-    </div> 
-</> 
-  )
+    return (
+        <>
+            <div className={avg ? 'communityRating' : 'stars'}>
+                {[...Array(5)].map((star, idx) => {
+                    idx += 1
+                    return (
+                        <span
+                            key={idx}
+                            className={`${styles.stars} ${idx <= starRating ? styles.on : styles.off}`}
+                        >
+                            &#9733;
+                        </span>
+                    )
+                })}
+            </div>
+        </>
+    )
 }
 
 export default Stars
--- src/Pages/AuthenticatePage.js
+++ src/Pages/AuthenticatePage.js
@@ -1,63 +1,64 @@
-import '../App.css';
-import Header from '../Components/Header.js';
-import MainPage from './MainPage.js';
-import SearchPage from './SearchPage.js';
-import ViewPage from './ViewPage.js';
-import UserLists from '../Components/UserLists.js';
-import LoginPage from './LoginPage.js';
-import UserContext from '../UserContext.js';
+import '../App.css'
+import Header from '../Components/Header.js'
+import MainPage from './MainPage.js'
+import SearchPage from './SearchPage.js'
+import ViewPage from './ViewPage.js'
+import UserLists from '../Components/UserLists.js'
+import LoginPage from './LoginPage.js'
+import UserContext from '../UserContext.js'
 
-import React, { useEffect, useContext } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useContext } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import axios from 'axios'
 
 function AuthenticatePage() {
-    const { authenticated, reRender, setAuthenticated, setUser } = useContext(UserContext);
+    const { authenticated, reRender, setAuthenticated, setUser } = useContext(UserContext)
 
     useEffect(() => {
-      axios.get(process.env.NODE_ENV === 'production'
-        ? process.env.REACT_APP_BACK_END_PROD + "/user/me"
-        : process.env.REACT_APP_BACK_END_DEV + "/user/me", 
-        {
-        withCredentials: true,
-        headers: { 
-          token: JSON.parse(localStorage.getItem('token'))
-        }
-      }).then(response => {
-        const {authenticated, user} = response.data;
-        setAuthenticated(authenticated);
-        setUser(user);
-      })
+        axios.get(process.env.NODE_ENV === 'production'
+            ? process.env.REACT_APP_BACK_END_PROD + "/user/me"
+            : process.env.REACT_APP_BACK_END_DEV + "/user/me",
+            {
+                withCredentials: true,
+                headers: { token: JSON.parse(localStorage.getItem('token')) }
+            }).then(response => {
+                const { authenticated, user } = response.data
+                setAuthenticated(authenticated)
+                setUser(user)
+            })
     }, [reRender])
 
-  return (
-    <>
-      {authenticated 
-      ? <div>
-        <header className='headerLayout'>
-          <Header />
-        </header>
-        <main>
-          <div className='bodyContainerLayout'>
-            <div className='bodyContentLayout'>
-              <Routes>
-                <Route path='/' element={<MainPage />} />
-                <Route path='/mybooks' element={<UserLists />} />
-                <Route path='/search' element={<SearchPage />} />
-                <Route path='/book/:id' element={<ViewPage />} />
-              </Routes>
-            </div>
-            <div className='userListsLayout'>
-              <UserLists />
-            </div>
-          </div>
-        </main>
-      </div> 
-      : <div>
-        <LoginPage />
-      </div>}
-    </>
-  );
+    return (
+        <>
+            {authenticated
+                ?
+                <div>
+                    <header className='headerLayout'>
+                        <Header />
+                    </header>
+                    <main>
+                        <div className='bodyContainerLayout'>
+                            <div className='bodyContentLayout'>
+                                <Routes>
+                                    <Route path='/' element={<MainPage />} />
+                                    <Route path='/mybooks' element={<UserLists />} />
+                                    <Route path='/search' element={<SearchPage />} />
+                                    <Route path='/book/:id' element={<ViewPage />} />
+                                </Routes>
+                            </div>
+                            <div className='userListsLayout'>
+                                <UserLists />
+                            </div>
+                        </div>
+                    </main>
+                </div>
+                :
+                <div>
+                    <LoginPage />
+                </div>
+            }
+        </>
+    )
 }
 
-export default AuthenticatePage;
+export default AuthenticatePage
